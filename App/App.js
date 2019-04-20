ed752fd311716af310a9882f8d6aa508b019afd0
Go Tae Geon <gtg7784@lab-c.me>
2019-04-21 04:25:56 +0900

    App

---
+++ App/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, View, WebView } from 'react-native';
+import { SafeAreaView } from 'react-navigation'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        <View style={{flex: 1, margin: 0, padding: 0}}>
+          <WebView useWebKit={true} source={{url: 'https://www.google.com'}} style={{flex: 1}}/>
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  webview: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
