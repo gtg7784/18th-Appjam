3fd8e6aa795a05b8a6d2d8f9558a7dd9a9c99c72
Go Tae Geon <gtg7784@lab-c.me>
2019-04-22 11:33:17 +0900

    wifi

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,18 +1,54 @@
-#include <Adafruit_NeoPixel.h>
+const char *ssid = "iphone";
+const char *password = "1234567890";
 
-#define PIN 6
+const char* host = "dimiwords.tk:30001";
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, PIN, NEO_GRB + NEO_KHZ800);
+void setup (){
+    Serial.begin(115200);
 
-int light = 225;
+    WiFi.disconnect(true);
+    delay(1000);
+    WiFi.onEvent(WiFiEvent);
+    WiFi.begin(ssid, password);
+    Serial.println("Wait for WiFi... ");
+    
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+void loop(){
+    wificonnect();
 
-void setup() {
-  Serial.begin(9600);
-  
-  strip.begin(); 
-  strip.show(); 
 }
 
-void loop() {
-  theaterChase(strip.Color(127, 127, 127), 50); //흰색 출력
+void wificonnect(){
+    delay(2000);
+    
+    WiFiClient client;
+    const int httpPort = 80;
+    if (!client.connect(host, httpPort)) {
+        Serial.println("connection failed");
+        return;
+    }
+
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+    "Host: " + host + "\r\n" + 
+    "Connection: close\r\n\r\n");
+  
+    while (client.available() == 0) {
+        if (timeout - millis() < 0) {
+        Serial.println(">>> Client Timeout !");
+        client.stop();
+        return;
+        }
+    }
+
+    while(client.available()){
+        String line = client.readStringUntil('\r');
+        Serial.print(line);
+    }
 }
